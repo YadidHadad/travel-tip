9254d6a0e98c0221558e9a71e5093879f97660a8
Nufar Maimon <nufar.maimon@gmail.com>
2022-11-02 21:50:15 +0200

    add weather

--- js/app.controller.js
+++ js/app.controller.js
@@ -1,74 +1,97 @@
 import { locService } from './services/loc.service.js';
 import { mapService } from './services/map.service.js';
+import { weatherService } from './services/weather.service.js';
 
-window.onload = onInit
-window.onAddMarker = onAddMarker
-window.onPanTo = onPanTo
-window.onGetLocs = onGetLocs
-window.onGetUserPos = onGetUserPos
-window.getPosition = getPosition
+window.onload = onInit;
+window.onAddMarker = onAddMarker;
+window.onPanTo = onPanTo;
+window.onGetLocs = onGetLocs;
+window.onGetUserPos = onGetUserPos;
+window.getPosition = getPosition;
 
-var gUserCurrPos
+var gUserCurrPos;
 
 // let gInfoWindow
 function onInit() {
-    onGetUserPos()
+  onGetUserPos()
+    .then(() => {
+      mapService
+        .initMap(locService.loadCurrPos().lat, locService.loadCurrPos().lng)
         .then(() => {
-            mapService.initMap(locService.loadCurrPos().lat, locService.loadCurrPos().lng)
-                .then(() => { onAddMarker(locService.loadCurrPos()) })
-            // .then(() => locService.addEventListener())
-        })
-        .catch(() => console.error('Error: cannot init map'))
+          onAddMarker(locService.loadCurrPos());
+        });
+      // .then(() => locService.addEventListener())
+    })
+    .catch(() => console.error('Error: cannot init map'));
 
-    // renderLocationTable()
+  // renderLocationTable()
 }
 
 // This function provides a Promise API to the callback-based-api of getCurrentPosition
 function getPosition() {
-    console.log('Getting Pos');
-    return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(resolve, reject);
-    });
+  console.log('Getting Pos');
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
 }
 
 function onAddMarker(pos) {
-    console.log('Adding a marker');
-    mapService.addMarker(pos);
+  console.log('Adding a marker');
+  mapService.addMarker(pos);
 }
 
 function onGetLocs() {
-    locService.getLocs().then((locs) => {
-        console.log('Locations:', locs);
-        document.querySelector('.locs').innerText = JSON.stringify(locs, null, 2);
-    });
+  locService.getLocs().then((locs) => {
+    console.log('Locations:', locs);
+    document.querySelector('.locs').innerText = JSON.stringify(locs, null, 2);
+  });
 }
 
 function onGetUserPos() {
-    return new Promise((resolve, reject) => {
-        resolve(getPosition()
-            .then((pos) => { return { lat: pos.coords.latitude, lng: pos.coords.longitude } })
-            .then(userPos => { locService.saveCurrLoc(userPos); return userPos })
-        )
-    })
+  return new Promise((resolve, reject) => {
+    resolve(
+      getPosition()
+        .then((pos) => {
+          return { lat: pos.coords.latitude, lng: pos.coords.longitude };
+        })
+        .then((userPos) => {
+          locService.saveCurrLoc(userPos);
+          return userPos;
+        })
+    );
+  });
 }
 
 function onPanTo(pos) {
-    console.log(`pos:`, pos)
-    console.log('Panning the Map');
-    // mapService.panTo(35.6895, 139.6917);
-    mapService.panTo(pos.lat, pos.lng);
+  console.log(`pos:`, pos);
+  console.log('Panning the Map');
+  // mapService.panTo(35.6895, 139.6917);
+  mapService.panTo(pos.lat, pos.lng);
 }
 
 function renderLocationTable() {
-    locService.getLocs()
-        .then(locs => {
-            const strHtmls = locs.forEach(({ name }) => {
-                return `<div class="location-item">
+  locService.getLocs().then((locs) => {
+    const strHtmls = locs.forEach(({ name }) => {
+      return `<div class="location-item">
                             <h2 class="loc-name">${name}</h2>
                         </div>
-                        `
-            })
-            document.querySelector('.location-table').innerHTML = strHtmls
-        }
-        )
+                        `;
+    });
+    document.querySelector('.location-table').innerHTML = strHtmls;
+  });
+}
+
+function renderWeather(data) {
+  var elWeather = document.querySelector('.weather');
+  elWeather.querySelector(
+    '.weather-location'
+  ).innerText = `${data.name}, ${data.weather[0].description}`;
+
+  var currTemp = kelvinToCel(data.main.temp);
+  var temp_min = kelvinToCel(data.main.temp_min);
+  var temp_max = kelvinToCel(data.main.temp_max);
+  elWeather.querySelector(
+    '.temp'
+  ).innerText = `${currTemp} tempature, from ${temp_min} to ${temp_max}`;
+  elWeather.querySelector('.wind').innerText = `${data.wind.speed} meter/sec`;
 }
