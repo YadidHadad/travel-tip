9f61fda5cb5241e206f4ce3f5d9e9ae59c5c4948
Nufar Maimon <nufar.maimon@gmail.com>
2022-11-02 16:16:28 +0200

    service update

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -1,19 +1,41 @@
+import { storageService } from './services/storage.service.js';
+import { utilService } from './services/util.service.js';
+
 export const locService = {
-    getLocs
-}
+  getLocs,
+};
 
+const STORAGE_KEY = 'locsDB';
+var gLocsCache = loadFromStorage(STORAGE_KEY);
+const locs ; 
 
-const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
-]
+const defaultLocs = [
+  { name: 'Greatplace', lat: 32.047104, lng: 34.832384 },
+  { name: 'Neveragain', lat: 32.047201, lng: 34.832581 },
+];
 
 function getLocs() {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(locs)
-        }, 2000)
-    })
+    if (gLocsCache) return Promise.resolve(gLocsCache);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(defaultLocs);
+    }, 2000);
+  });
 }
 
+//function that create a new Loc, and adding it to the Locs array
+function setLocs(id= makeId(), name, lat, lng, createdAt= Date.now()) {
+    // var locs = [{ id: 1, name: home, lat: 11.22, lng: 22.11, createdAt: 1519211810362 }]
+    if (lat && lng) locs = [{ id, name, lat, lng, createdAt}];
+    return locs;
+  }
+
+//we got the starter with this func
+// function getLocs() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(locs);
+//     }, 2000);
+//   });
+// }
 
