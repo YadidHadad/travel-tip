6a60fe42a304cd104b6bfd9d83f013f335dcc4fc
YadidHadad <102486082+YadidHadad@users.noreply.github.com>
2024-05-02 18:34:29 +0300

    Update weather.service.js

--- js/services/weather.service.js
+++ js/services/weather.service.js
@@ -3,7 +3,8 @@ export const weatherService = {
 
 }
 
-const WEATHER_API = '${secrets.WEATHER_KEY}'
+const WEATHER_API = secrets.WEATHER_KEY
+console.log(WEATHER_API)
 // const WEATHER_API = 'cca6c2803bb413e19be59f3727b3bf01'
 
 function getWeather(lat = 34.0, lng = 35.0) {
