7f7d35df2372b62538fc595158d7082a14390abf
YadidHadad <102486082+YadidHadad@users.noreply.github.com>
2024-05-02 23:49:53 +0300

    Update weather.service.js

--- js/services/weather.service.js
+++ js/services/weather.service.js
@@ -4,7 +4,8 @@ export const weatherService = {
 }
 console.log(process.env.WEATHER_API)
 
-const WEATHER_API = process.env.WEATHER_KEY
+const WEATHER_API = 'API_KEY_PLACEHOLDER';
+// const WEATHER_API = process.env.WEATHER_KEY
 // const WEATHER_API = 'cca6c2803bb413e19be59f3727b3bf01'
 
 function getWeather(lat = 34.0, lng = 35.0) {
