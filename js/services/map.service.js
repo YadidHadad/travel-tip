0616b533f13ab255f3e0e0ee12e098e53ceaa950
YadidHadad <102486082+YadidHadad@users.noreply.github.com>
2024-05-03 18:26:02 +0300

    Update map.service.js

--- js/services/map.service.js
+++ js/services/map.service.js
@@ -36,8 +36,8 @@ function initMap(lat = 32.0749831, lng = 34.9120554, MAPS_KEY) {
             console.log('Map!', gMap);
 
             return gMap
-        })
-        .catch(console.log('Error: initMap');
+        }
+        .catch(console.log('Error: initMap'))
 }
 
 function getMap() {
